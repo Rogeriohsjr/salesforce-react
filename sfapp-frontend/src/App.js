97b2fdd6c5b42cc492b3b0aee4bad6243f9b17bd
Rogerio <rogerio.dasilva@formstack.com>
2022-07-30 09:58:42 -0400

    added redux and one simple storage.

--- sfapp-frontend/src/App.js
+++ sfapp-frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Counter } from "./features/counter/Counter";
 
 function App() {
   return (
     <div>
       <h2>Welcome to React App</h2>
       <h3>Date : {new Date().toDateString()}</h3>
+      <Counter></Counter>
     </div>
   );
 }
--- sfapp-frontend/src/index.js
+++ sfapp-frontend/src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
-import ReactDom from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./App.css";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
-ReactDom.render(<App />, document.getElementById("app"));
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
